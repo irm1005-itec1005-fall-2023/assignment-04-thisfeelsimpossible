054163042951933b904b337c0b083f9218f2f138
thisfeelsimpossiblee <kiranbutter@cmail.carleton.ca>
2023-11-26 23:29:25 -0500

    better than last time

--- script.js
+++ script.js
@@ -39,36 +39,16 @@
 
 // all taken from: https://m.youtube.com/watch?v=G0jO8kUrg-I&pp=ygUGI3l1ZGl2
 
-let inputBox = document.getElementById("input-box");
-let listContainer = document.getElementById("list-container");
 
-function addTask() {
-  //if(inputBox.value === '') {
-    //alert("Write something");
-  //}
-  //else { 
-    {
-    let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    let button = document.createElement("button");
-    button.innerHTML = "\u00d7";
-    li.appendChild(button);
-  }
-  inputBox.value = "";
-  saveData();
-}
 
-listContainer.addEventListener("click", function(event) {
-  if(event.target.tagName === "LI"){
-    event.target.classList.toggle("checked");
-    saveData();
-  }
-  else if(event.target.tagName === "BUTTON") {
-    event.target.parentElement.remove();
-    saveData();
-  }
-}, false);
+
+
+
+
+
+
+
+
 
 function saveData(){
   localStorage.setItem("data", listContainer.innerHTML);
